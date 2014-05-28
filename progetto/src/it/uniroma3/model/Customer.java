4cbac32b510ab283656df9ffbd76361929edb73d
leomagnus <leonardoceppi@10.25.1.141>
2014-05-28 15:31:18 +0200

    ciao

--- progetto/src/it/uniroma3/model/Customer.java
+++ progetto/src/it/uniroma3/model/Customer.java
@@ -10,11 +10,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
