dba603caa137c551fde22f3f5d4f23695eae52f0
Chiara19 <chiara_lombardi@hotmail.it>
2014-05-28 15:31:32 +0200

    gggg

---
+++ progetto/src/it/uniroma3/model/Administrator.java
@@ -0,0 +1,64 @@
+package it.uniroma3.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Administrator {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String firstName;
+	@Column(nullable = false)
+	private String lastName;
+	private String password;
+	
+	public Administrator(){
+		
+	}
+	
+	public Administrator(String firstName, String lastName, String password){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrator [id=" + id + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", password=" + password + "]";
+	}
+
+}
